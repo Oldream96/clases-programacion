e9dac99f7e4624d897b0ba5917fe0ecf8e046eda
Diego Contreras <diegocont18@gmail.com>
2025-12-24 09:11:53 -0500

    inicio del calculo de area

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
+import { Triangulo } from './ejercicios/triangulo-ejec';
 console.log('modificacion version 2');
 
 console.log('modificacion version 3');
 console.log('modificacion version 4');
+
+var triangulo = new Triangulo(5,4);
