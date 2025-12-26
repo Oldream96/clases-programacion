09094e9ac420c4d9082d4f6df3f3d05f6966367e
Diego Contreras <diegocont18@gmail.com>
2025-12-26 08:27:41 -0500

    cambios en app.ts calculo triangulo area

--- app.ts
+++ app.ts
@@ -2,3 +2,8 @@ import { Triangulo } from './ejercicios/triangulo-ejec';
 console.log('modificacion version 2');
 
 var triangulo = new Triangulo(5,4);
+
+var triangulo2 = new Triangulo(5,4);
+var triangulo3 = new Triangulo(5,4);
+
+console.log("el area del triangulo 1 es: ", triangulo.calcularArea());
